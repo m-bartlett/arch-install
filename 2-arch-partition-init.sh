719b176de8115fab092d151e6955c429b6d83921
M B <85039141+m-bartlett@users.noreply.github.com>
2021-11-09 21:46:44 -0700

    automate LUKS crypt passphrase usage

--- 2-arch-partition-init.sh
+++ 2-arch-partition-init.sh
@@ -4,6 +4,8 @@ set -o nounset
 set -o pipefail
 trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 
+BASH_SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 read -p "Enter hostname: " hostname
 : ${hostname:?"hostname cannot be empty"}
 
@@ -24,6 +26,23 @@ while : ; do
     break
   else
     echo "Passwords did not match"
+  fi
+done
+
+while : ; do
+  read -p "Enter LUKS crypt passphrase: " cryptpassphrase
+  printf '\e[1F\e[2K'
+  if [ -z "$cryptpassphrase" ]; then
+    echo "Crypt passphrase cannot be empty"
+    continue
+  fi
+
+  read -p "Enter LUKS crypt passphrase again: " cryptpassphrase2
+  printf '\e[1F\e[2K'
+  if [[ "$cryptpassphrase" == "$cryptpassphrase2" ]]; then
+    break
+  else
+    echo "Crypt passphrase did not match"
   fi
 done
 
@@ -49,6 +68,7 @@ cryptfsname="${cryptfsname:-cryptroot}"
 
 clear
 cat <<EOF
+
 hostname  = $hostname
 username  = $user
 device    = $device
@@ -62,6 +82,8 @@ if [ "${input,,}" != "y" ]; then
   echo "Quitting..."
   exit 1
 fi
+
+export hostname user password device cryptfsname
 
 
 set -x
@@ -88,9 +110,9 @@ parted \
 
 ### Setup the disk and partitions ###
 
-part_boot="$(ls ${device}* | grep -E "^${device}p?1$")"
-part_swap="$(ls ${device}* | grep -E "^${device}p?2$")"
-part_root="$(ls ${device}* | grep -E "^${device}p?3$")"
+export part_boot="$(ls ${device}* | grep -E "^${device}p?1$")"
+export part_swap="$(ls ${device}* | grep -E "^${device}p?2$")"
+export part_root="$(ls ${device}* | grep -E "^${device}p?3$")"
 
 wipefs "${part_boot}"
 wipefs "${part_swap}"
@@ -104,8 +126,8 @@ mkfs.vfat -F32 -n "EFI" "${part_boot}"
 
 modprobe dm-crypt
 modprobe dm-mod
-cryptsetup -y -v luksFormat -s 512 -h sha512 "${part_root}"
-cryptsetup open "${part_root}" ${cryptfsname}
+cryptsetup -y -v luksFormat -s 512 -h sha512 "${part_root}" <<<"$cryptpassphrase"$'\n'"$cryptpassphrase"
+cryptsetup open "${part_root}" ${cryptfsname} <<<"$cryptpassphrase"
 mkfs.ext4 -L root /dev/mapper/${cryptfsname}
 
 
@@ -117,46 +139,16 @@ swapon "${part_swap}"
 
 mount /dev/mapper/${cryptfsname} /mnt
 mkdir -p /mnt/boot/efi
-mount "${part_boot}" /mnt/boot/efi
-
-# genfstab -U /mnt > /mnt/etc/fstab
 mkdir /mnt/etc
-genfstab -t PARTUUID /mnt > /mnt/etc/fstab
-
-echo "${hostname}" > /mnt/etc/hostname
-printf "127.0.0.1\tlocalhost\t${hostname}\n::1\tlocalhost\t${hostname}" >> /etc/hosts
+mount "${part_boot}" /mnt/boot/efi
 
 pacstrap /mnt base base-devel efibootmgr grub linux linux-firmware
 
+genfstab -pU /mnt >> /mnt/etc/fstab
 
+cp -v $BASH_SOURCE_DIR/3-*.sh /mnt/arch-init.sh
+arch-chroot /mnt /arch-init.sh
 
-
-#### Init installed root
-
-sed -i 's/#\(en_US\.UTF-8\)/\1/' /mnt/etc/locale.gen
-# sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /mnt/etc/ssh/sshd_config
-sed -i "s/#Server/Server/g" /mnt/etc/pacman.d/mirrorlist
-# sed -i 's/#\(Storage=\)auto/\1volatile/' /mnt/etc/systemd/journald.conf
-sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /mnt/etc/systemd/logind.conf
-sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /mnt/etc/systemd/logind.conf
-sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /mnt/etc/systemd/logind.conf
-
-sed -i 's@\(^GRUB_CMDLINE_LINUX=\)""@\1"cryptdevice=/dev/sda3:cryptroot:allow-discards"@' /mnt/etc/default/grub
-sed -i -E -e 's/(^HOOKS=\(.*) filesystems/\1 encrypt filesystems/' /mnt/etc/mkinitcpio.conf
-echo LANG=en_US.UTF-8 > /mnt/etc/locale.genconf
-# export LANG='en_US.UTF-8'
-
-arch-chroot /mnt useradd -mU -s /bin/bash -G audio,games,input,lp,network,power,root,storage,sys,uucp,video,wheel "$user"
-echo "$user:$password" | chpasswd --root /mnt
-echo "root:$password" | chpasswd --root /mnt
-arch-chroot /mnt  locale-gen
-arch-chroot /mnt  ln -sf /usr/share/zoneinfo/US/Mountain /etc/localetime
-arch-chroot /mnt  hwclock --systohc --utc
-arch-chroot /mnt  timedatectl set-ntp true
-arch-chroot /mnt  mkinitcpio -p linux
-arch-chroot /mnt  grub-install --boot-directory=/boot --efi-directory=/boot/efi "${part_boot}"
-arch-chroot /mnt  grub-mkconfig -o /boot/grub/grub.cfg
-arch-chroot /mnt  grub-mkconfig -o /boot/efi/EFI/arch/grub.cfg
 
 clear
 echo "Done!"
---
+++ 3-arch-chroot-init.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+
+echo "${hostname}" > /etc/hostname
+printf "127.0.0.1\tlocalhost\t${hostname}\n::1\tlocalhost\t${hostname}" >> /etc/hosts
+
+safe-sed() {  # sed instructions should be idempotent!
+  local sed_instruction="$2" file="$3"
+  if [ -f "$file" ]; then
+    sed -i "$sed_instruction" "$file"
+  else
+    echo "sed: file '$file' does not exist"
+  fi
+}
+
+# sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
+safe-sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
+safe-sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
+safe-sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
+safe-sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
+safe-sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
+safe-sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
+
+safe-sed -i 's@\(^GRUB_CMDLINE_LINUX=\)""@\1"cryptdevice=/dev/sda3:cryptroot:allow-discards"@' /etc/default/grub
+safe-sed -i 's/\(^HOOKS=(.*\) filesystems/\1 encrypt filesystems/' /etc/mkinitcpio.conf
+
+echo LANG=en_US.UTF-8 > /etc/locale.genconf
+export LANG='en_US.UTF-8'
+locale-gen
+
+ln -sf /usr/share/zoneinfo/US/Mountain /etc/localetime
+hwclock --systohc --utc
+timedatectl set-ntp true
+
+useradd -mU -s /bin/bash -G audio,games,input,lp,network,power,root,storage,sys,uucp,video,wheel "$user"
+chpasswd <<<"$user:$password"
+chpasswd <<<"root:$password"
+
+mkinitcpio -p linux
+grub-install --boot-directory=/boot --efi-directory=/boot/efi "${part_boot}"
+grub-mkconfig -o /boot/grub/grub.cfg
+grub-mkconfig -o /boot/efi/EFI/arch/grub.cfg
